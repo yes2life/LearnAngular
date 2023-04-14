c5691be8d087aa450859b3dda0eaf2c58280345e
Steven Watts <StevenDoesIT@gmail.com>
2023-04-14 01:11:54 -0700

    Completed Section 2.39.

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -10,7 +10,15 @@ export class ServerComponent {
 	serverId: number 		= 10;
 	serverStatus: string  	= 'offline';
 
-	getServerStatus() {
+	constructor() {
+		this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+	}
+
+	getServerStatus(): string {
 		return this.serverStatus;
 	}
+
+	getColor() {
+		return this.serverStatus === 'online' ? 'green' : 'red';
+	  }
 }
