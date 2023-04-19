f1295746dea29612dc59838bef31c2308a5a94ad
Steven Watts <StevenDoesIT@gmail.com>
2023-04-19 01:10:38 -0700

    Lesson 53 not working.

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-
+export class RecipeListComponent  {
+  recipes: Recipe[] = [
+      new Recipe(
+        'A Test Recipe',
+        'This is simply a test',
+        'https://p1.pxfuel.com/preview/217/260/0/zucchini-summer-squash-food-recipe-delicious-breakfast.jpg'
+      )
+    ];
+  
+  constructor() {
+    // console.log(this.recipes.toString);
+  }
 }
