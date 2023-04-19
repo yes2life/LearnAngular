e00a81de699cfda5f94ffc202914a907f0869b31
Steven Watts <StevenDoesIT@gmail.com>
2023-04-19 15:05:22 -0700

    Completed 5.67, binding to child component properties.

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  serverElements = [];
+  // serverElements = [];
+
+  // this @Input() decorator allows the element property to be accessed outside of this component
+  @Input('srvElement') element: { type: string, name: string, content: string };
+
   constructor() { }
 
   ngOnInit(): void {
