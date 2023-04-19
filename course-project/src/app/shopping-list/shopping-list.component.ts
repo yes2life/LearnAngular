9496f3e739915c120ffd7fa21c9e9b32aa1409c4
Steven Watts <StevenDoesIT@gmail.com>
2023-04-19 13:27:38 -0700

    Completed Section 3.56.

--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients = [];
 
+  constructor() {
+
+    
+  }
 }
