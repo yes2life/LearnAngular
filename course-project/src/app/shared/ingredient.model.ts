9f9e0734ad425aeb1f5bbec9f216999645f01a9b
Steven Watts <StevenDoesIT@gmail.com>
2023-04-19 13:33:05 -0700

    Completed Section 3. 57.
    Contains abbreviated constructor code.

---
+++ course-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredients {
+	// // long way
+	// public name: string;
+	// public amount: number;
+
+
+	// constructor(name: string, amount: string) {
+	// 	this.name = name;
+	// 	this.amount = amount;
+	// }
+
+	/**
+	 *
+	 */
+	constructor(public name: string, public amount: number) {}
+}
--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -9,7 +9,6 @@ export class ShoppingListComponent {
   ingredients = [];
 
   constructor() {
-
     
   }
 }
