aac3a13cad6b0e85318973a9a377fa6cbc5a9d6f
Steven Watts <StevenDoesIT@gmail.com>
2023-04-23 14:17:13 -0700

    Completed Section 5.79, Lifecycle hooks.

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -24,5 +24,14 @@ export class AppComponent {
       name: blueprintData.serverName,
       content: blueprintData.serverContent
     });
-  }    
+  }  
+  
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
+  
 }
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,20 +1,73 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  OnChanges, 
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+    OnInit, 
+    OnChanges, 
+    DoCheck, 
+    AfterContentInit, 
+    AfterContentChecked, 
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
   // serverElements = [];
 
   // the element variable is set to a type using an object
   // this @Input([alias]) decorator allows the element property to be accessed outside of this component
   @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
   }
 
 }
